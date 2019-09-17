732b938b3e46f9b88117089c5fe88198a0045f9c
mangesh <mvadkte@gmail.com>
2019-09-17 18:48:17 +0530

    New Class Added And Its Name DuplicateElement.java id-1010

---
+++ DuplicateElement.java
@@ -0,0 +1,12 @@
+package com.nt.resource;
+
+public class DuplicateElement {
+
+	public static void main(String[] args) {
+		int a[] = { 1, 2, 5, 6, 5, 6, 2, 7 };
+		for (int i = 0; i < a.length - 1; i++)
+			for (int j = i + 1; j < a.length; j++)
+				if ((a[i] == a[j]) && (i != j)) 
+					System.out.println(a[j]);      
+	    }
+}
